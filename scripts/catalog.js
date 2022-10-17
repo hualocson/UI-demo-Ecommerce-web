db7ad67c8c8cdadd09785d014d84e9c8619147a8
LocSon <hualocson@gmail.com>
2022-10-17 11:56:45 +0700

    Modify url

--- scripts/catalog.js
+++
@@ -1,9 +0,0 @@
-function displayFiction(){
-    document.getElementById("fiction-form").style.display = "block";
-}
-function displayNonFiction(){
-    document.getElementById("non-fiction-form").style.display = "block";
-}
-function displayChildren(){
-    document.getElementById("children-form").style.display = "block";
-}
